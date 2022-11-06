d9c3148abf445f0e268d5989245facfb542cff82
Lucas Winningham <lucas.winningham@gmail.com>
2022-11-06 01:02:00 -0400

    First commit.

---
+++ lib/intention/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Intention
+  VERSION = '0.1.0'
+end
---
+++ spec/intention_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Intention do
+  it "has a version number" do
+    expect(Intention::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
